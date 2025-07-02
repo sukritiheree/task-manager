81608e2d0c561f5ce24b9478a819c428e5ad7b1e
Sukriti Kanauji <sukritikanauji@gmail.com>
2025-07-02 18:33:35 +0530

    temp changes

---
+++ frontend/src/pages/Admin/CreateTask.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const createTask = () => {
+  return (
+    <div>createTask</div>
+  )
+}
+
+export default createTask
--- frontend/src/pages/temp/CreateTask.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const createTask = () => {
-  return <div>CreateTask</div>;
-};
-
-export default CreateTask;
